c9db02fe3a712f366a90829f6483cd5dec078f1a
krisherod <herodkris@gmail.com>
2017-04-05 20:06:53 -0400

    insight application

---
+++ src/features/hours.py
@@ -0,0 +1,39 @@
+'''
+this function finds the top 10 busiest hours
+'''
+
+# dependencies
+from datetime import datetime
+import bisect
+
+
+def get_hours (log_file, hours_path):
+
+	if not log_file.load: return False
+
+	dates_file = log_file.dates
+	timezone = log_file.timezone
+
+	frequency = []
+
+	# iterate through all possible times between the ranges
+	for i, timestamp in enumerate(range(dates_file[0], dates_file[-1])):
+
+		# get the value of the next hour
+		next_hour = timestamp + 3600
+
+		# use the bisect method to get the index of the list which contains a timestamp greater than the next hour
+		# subtract 1 so that you get the value greater than or equal to the first value
+		i1 = bisect.bisect(dates_file, timestamp-1)
+		i2 = bisect.bisect(dates_file, next_hour)
+
+		# create a tuple with the timestamp of the start of the busiest hour, the index, and the difference
+		frequency.append((timestamp, i1, i2-i1))
+
+
+	with open(hours_path, 'w') as hours_logfile:
+		for freq in sorted(frequency, key=lambda x: x[2], reverse=True)[:10]:
+			hours_logfile.write('%s %s,%d\n' %(str(datetime.fromtimestamp(freq[0]).strftime('%d/%b/%Y:%H:%M:%S')), timezone[freq[1]] ,freq[2]))
+
+
+	return True
